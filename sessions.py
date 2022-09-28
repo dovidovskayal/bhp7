39c903e99484ad48e15dddbe0ec8f8e2d7b627dc
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-09-28 18:25:27 +0300

    initial

--- sessions.py
+++ sessions.py
@@ -1,13 +1,15 @@
 from aiohttp import ClientSession
 import asyncio
 
+
 async def get_wss():
     async with ClientSession() as session:
         async with session.ws_connect(
-            url='wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self'
+                url='wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self'
         ) as wss:
             async for message in wss:
                 print(message.data)
-                #await wss.send_str('in')
+                # await wss.send_str('in')
+
 
 asyncio.run(get_wss())
