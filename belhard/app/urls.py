54042849a001bf36b81441d075ff82396dc157d3
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-09-30 20:16:19 +0300

    initial

---
+++ belhard/app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, register_converter, re_path
+from .views import index
+from django.conf.urls import handler404
+
+
+
+class YearConverter:
+    regex = '[0-9]{4}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return str(value)
+
+
+register_converter(YearConverter, 'yyyy')
+
+urlpatterns = [
+    path('category/<int:category_id>', index),
+]
+
+handler404 = 'app.views.error404'
--- belhard/app/views.py
+++ belhard/app/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.http import require_GET
+from .models import Category
+from django.shortcuts import render, get_object_or_404
+
+
+@require_GET
+def index(request: HttpRequest, category_id: int):
+    category = get_object_or_404(Category, id=category_id)
+    # if not category:
+    #     raise Http404
+    return HttpResponse('<b>HELLO</b>')
+
+
+def error404(request, exception):
+    return HttpResponse('<b>404</b>')
